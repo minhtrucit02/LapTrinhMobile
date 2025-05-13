8df8be41c0e45bab0b734d8f549ae9fbc534924b
NgTruc <nguyenminhtruc29022004@gmail.com>
2025-05-14 00:02:36 +0700

    baiTapTuan1

---
+++ ex_week1/lib/Ex_Week1/ExerciseWeek1.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+class ExerciseWeek1 extends StatelessWidget{
+  const ExerciseWeek1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _iconButton(Icons.arrow_back),
+                    _iconButton(Icons.edit),
+                  ],
+                ),
+                const SizedBox(height: 250),
+                const CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/avatar.png'),
+                ),
+                const SizedBox(height: 20,),
+                const Text(
+                  'NguyenMinhTruc',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8,),
+                const Text(
+                  'BinhDinh, VietNam',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  Widget _iconButton(IconData icon){
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+          onPressed: (){},
+          icon: Icon(icon, color: Colors.black,)),
+    );
+  }
+}
+
---
+++ ex_week1/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:ex_week1/Ex_Week1/ExerciseWeek1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exercise Moblie',
+      home: ExerciseWeek1(),
+    );
+  }
+}
